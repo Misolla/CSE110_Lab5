cf45ef2117989f5d7f13f09f006cc3e7c943b46f
MinQuach <m2quach@ucsd.edu>
2024-11-02 17:37:59 -0700

    testing branch: fixed

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  // const titleElement = screen.getByText(/my budget planner/i);
-  const titleElement = screen.getByText(/my planner/i);
+  const titleElement = screen.getByText(/my budget planner/i);
   expect(titleElement).toBeInTheDocument();
 });
